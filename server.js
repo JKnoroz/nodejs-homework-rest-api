01f7362f28b05b4281b12a593c14e65943a40bc1
Yuliya <jknoroz@gmail.com>
2022-01-25 16:35:54 +0200

    bd

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
-const app = require("./app");
+// const app = require("./app");
 
-app.listen(3000, () => {
-  console.log(`Server running. Use our API on port: 3000`);
-});
+// app.listen(3000, () => {
+//   console.log(`Server running. Use our API on port: 3000`);
+// });
+
+const mongoose = require("mongoose");
+
+const { DB_HOST } = require("./config");
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => console.log("Database connection successful"))
+  .catch((error) => console.log(error.message));
