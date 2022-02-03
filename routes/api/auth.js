3f36142c94d71a7530a71b1f1582347fa50ac3ad
Yuliya <jknoroz@gmail.com>
2022-02-03 22:43:50 +0200

    auth

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const CreateError = require("http-errors");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const { User, schemas } = require("../../models/user");
 const router = express.Router();
+
+const { SECRET_KEY } = process.env;
 
 router.post("/register", async (req, res, next) => {
   try {
@@ -12,13 +16,48 @@ router.post("/register", async (req, res, next) => {
         "Ошибка от Joi или другой библиотеки валидации"
       );
     }
-    const [email, password] = req.body;
+    const { email, password } = req.body;
     const user = await User.findOne({ email });
     if (user) {
       throw new CreateError(409, "Email in use");
     }
-    const result = await User.create(req.body);
+    const salt = await bcrypt.genSalt(10);
+    const hashPassword = await bcrypt.hash(password, salt);
+    await User.create({ email, password: hashPassword });
     res.status(201).json({
+      user: {
+        email,
+      },
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post("/login", async (req, res, next) => {
+  try {
+    const { error } = schemas.register.validate(req.body);
+    if (error) {
+      throw new CreateError(
+        400,
+        "Ошибка от Joi или другой библиотеки валидации"
+      );
+    }
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) {
+      throw new CreateError(401, "Email or password is wrong");
+    }
+    const compareResult = await bcrypt.compare(password, user.password);
+    if (!compareResult) {
+      throw new CreateError(401, "Email or password is wrong");
+    }
+    const payload = {
+      id: user._id,
+    };
+    const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "1h" });
+    res.json({
+      token,
       user: {
         email,
       },
