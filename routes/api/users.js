6d307deac31808bdc64ec4af8c8b47a19ece148e
Yuliya <jknoroz@gmail.com>
2022-02-11 22:48:12 +0200

    avatar done

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const CreateError = require("http-errors");
+const fs = require("fs/promises");
+const path = require("path");
+const Jimp = require("jimp");
 
 const { User } = require("../../models/user");
 const { authenticate, upload } = require("../../middlewares");
@@ -37,11 +40,37 @@ router.patch("/updatesubscription", authenticate, async (req, res, next) => {
   }
 });
 
+const avatarsDir = path.join(__dirname, "../../", "public", "avatars");
+
 router.patch(
   "/avatars",
   authenticate,
   upload.single("avatar"),
-  async (req, res, next) => {}
+  async (req, res, next) => {
+    const { _id } = req.user;
+    const { path: tempUpload, filename } = req.file;
+    try {
+      await Jimp.read(tempUpload)
+        .then((image) => {
+          return image.resize(250, 250).write(tempUpload);
+        })
+        .catch((err) => {
+          console.error(err);
+        });
+
+      const [extention] = filename.split(".").reverse();
+      const newFileName = `${_id}.${extention}`;
+      const resultUpload = path.join(avatarsDir, newFileName);
+      await fs.rename(tempUpload, resultUpload);
+      const avatarURL = path.join("avatars", newFileName);
+      await User.findByIdAndUpdate(_id, { avatarURL });
+      res.json({
+        avatarURL,
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
 );
 
 module.exports = router;
